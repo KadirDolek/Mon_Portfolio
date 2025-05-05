35c7136c007bc3ae06ff62acb8b3f56cd9d25853
kadirdolek <kadirdolek@outlook.com>
2025-05-05 19:42:44 +0200

    portfolio

---
+++ public/js/main.js
@@ -0,0 +1,26 @@
+const body = document.querySelector('body')
+document.body.style.background = 'linear-gradient(to left, wheat, white, wheat)';
+const lien1 = document.getElementsByClassName('img-container')[0]
+const lien2 = document.getElementsByClassName('img-container')[1]
+const lien3 = document.getElementsByClassName('img-container')[2]
+
+lien1.addEventListener('click',()=>{
+    window.location.href="https://kadirdolek.github.io/CBienBon-KD/"
+})
+lien2.addEventListener('click',()=>{
+    window.location.href="https://kadirdolek.github.io/Projet-Romyk_KD/"
+})
+lien3.addEventListener('click',()=>{
+    window.location.href="https://kadirdolek.github.io/Calculette-KD/"
+})
+
+
+
+
+
+
+
+
+ 
+
+
